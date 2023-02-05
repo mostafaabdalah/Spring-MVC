4aba739996ab8033b6bc55478dd17bfffd1162ce
Mostafa abdallah <34196002+mostafaabdalah@users.noreply.github.com>
2023-02-05 23:57:12 +0200

    Form with MVC Form Tags

---
+++ src/main/java/com/spring/studentcontrol/StudentControllerMvcTags.java
@@ -0,0 +1,30 @@
+package com.spring.studentcontrol;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.model.Student;
+
+
+// http://localhost:8080/spring-mvc/student-mvc-tags
+@Controller
+@RequestMapping("/student-mvc-tags")
+public class StudentControllerMvcTags {
+
+	
+	//http://localhost:8080/spring-mvc/student-mvc-tags/account-Mvc-tags
+	@RequestMapping("/account-Mvc-tags")
+	public String StudentFormMvcTags(Model model) {
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return "student-front-mvc-tags/student-account";
+	}
+	
+	
+	@RequestMapping("/processmvctags")
+	public String ShowInfo(@ModelAttribute("student") Student student) {
+		return "student-front-mvc-tags/showdatastudent";
+	}
+}
